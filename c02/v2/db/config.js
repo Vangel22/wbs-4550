2805a54c30226ff9ed268c9191841849653b143a
Vangel22 <h.vangel22@gmail.com>
2025-09-01 21:52:52 +0200

    Deleted uri

--- c02/v2/db/config.js
+++ c02/v2/db/config.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 // ODM - Object data mapping - Non-relational DB
 // ORM - Object relation mapper -Relational DB
 
-const uri =
-  "mongodb+srv://Vangel22:test1234@cluster0.12jzasd.mongodb.net/grupa-4548?retryWrites=true&w=majority";
-
+const uri = "";
 async function connect() {
   try {
     await mongoose.connect(uri);
